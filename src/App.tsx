9e99e6dffd96ca84c5ff2c743b124bdce5931cc0
mzhaonanq <1002604003@qq.com>
2020-09-06 22:37:31 +0800

    初步完成底部导航栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,87 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
 
-function App() {
+const Wrapper = styled.div`
+  border:1px solid red;
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+  
+`
+const Main = styled.div`
+  flex-grow: 1;
+`
+const Nav = styled.nav`
+  border:1px solid blue ;
+  > ul{
+  display:flex;
+    > li{
+    width: 33.33%;
+    text-align: center;
+    padding: 16px;
+    }
+  }
+`
+
+ function App() {
   return (
-    <div>hi</div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Redirect from="/" exact to="/money" />
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/">记账</Link>
+            </li>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/statistics">数据</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Statistics() {
+  return <h2>数据页面</h2>;
+}
+
+function NoMatch(){
+  return <div>当前页面不存在</div>
+}
+export default App
