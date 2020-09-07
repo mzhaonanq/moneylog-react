5edda093710c3441bbcfcced3a539941a8b74c83
mzhaonanq <1002604003@qq.com>
2020-09-07 13:23:33 +0800

    完成Nav组件的3个icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/labels.svg')
+require('icons/money.svg')
+require('icons/statistics.svg')
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -10,7 +13,14 @@ const NavWrapper = styled.nav`
     > li{
     width: 33.33%;
     text-align: center;
-    padding: 16px;
+    padding: 4px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+      > .icon{
+         width: 24px;
+         height: 24px;
+      }
     }
   }
 `
@@ -20,12 +30,21 @@ const Nav =()=>{
     <NavWrapper>
       <ul>
         <li>
+          <svg   className='icon'>
+            <use xlinkHref='#money' />
+          </svg>
           <Link to="/">记账</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#labels' />
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#statistics'/>
+          </svg>
           <Link to="/statistics">数据</Link>
         </li>
       </ul>
