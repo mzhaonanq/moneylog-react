781ec4d4688ed8acf8e318d91fb6a339a6f9920f
mzhaonanq <1002604003@qq.com>
2020-09-09 16:10:19 +0800

    创建Input组件

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -4,20 +4,32 @@ import {useParams} from 'react-router-dom';
 import Layout from 'components/Layout';
 import Icon from '../components/Icon';
 import {Button} from '../components/Button';
+import styled from 'styled-components';
 
 type Params = {
   id: string
 }
+
+const Topbar = styled.header`
+display:flex;
+justify-content: space-between;
+align-items: center;
+line-height: 20px;
+padding: 14px;
+background: #ffffff;
+`
+
 const Tag: React.FunctionComponent = (props) => {
   const {findTag} = useTags();
   let {id} = useParams<Params>();
   const tag = findTag(parseInt(id));
   return (
     <Layout>
-      <header>
+      <Topbar>
         <Icon name='left'/>
         <span>编辑标签</span>
-      </header>
+        <Icon/>
+      </Topbar>
       <div>
       <label>
         <span>标签名</span>
--- src/views/money/NoteSection.tsx
+++ src/views/money/NoteSection.tsx
@@ -1,25 +1,11 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from 'components/Input';
 
 const Wrapper =styled.section`
   background: #f5f5f5;
   padding:10px 16px;
   font-size: 14px;
-  > label{
-  display: flex;
-  align-items: center;
-    >span{
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    >input{
-      display:block;
-      width: 100%;
-      height: 56px;
-      background:none;
-      border:none;
-    }
-  }
 `
 type Props ={
   value: string;
@@ -27,24 +13,15 @@ type Props ={
 }
 const NoteSection: React.FunctionComponent<Props> =(props)=>{
   const note = props.value
-  const refInput = useRef<HTMLInputElement>(null)
-  const onBlur =()=>{
-    if(refInput.current!==null){
-      props.onChange(refInput.current.value)
+  const onChange: ChangeEventHandler<HTMLInputElement> =(e)=>{
+      props.onChange(e.target.value)
     }
-  }
   return(
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type="text" placeholder='写点啥吧'
-               ref={refInput}
-               defaultValue={note}
-               onBlur={onBlur}
-        />
-      </label>
+      <Input label='备注' type='text' defaultValue={note} onChange={onChange} >
+
+      </Input>
     </Wrapper>
   )
 }
-
 export {NoteSection}
