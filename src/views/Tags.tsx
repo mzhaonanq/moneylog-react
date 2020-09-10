17380a679562177681eeff82e186765668ca3f3b
mzhaonanq <1002604003@qq.com>
2020-09-10 13:53:02 +0800

    完成tags的数据持久化

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout';
 import React from 'react';
-import {useTags} from '../useTags';
+import {useTags} from '../hooks/useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
 import {Link} from 'react-router-dom';
@@ -27,15 +27,16 @@ background: #fff;
   }
 `
 
+
 function Tags() {
-  const {tags} =   useTags()
+  const {tags,addTag} =   useTags()
   return(
     <Layout>
       <TagList>
       {tags.map(tag=>
         <li key={tag.id}>
           <Link to={'/tags/' +tag.id}>
-            <span className='oneLine'>{tag.name}</span>
+            <span className='oneLine'>{tag.id}:{tag.name}</span>
             <Icon name='right'/>
           </Link>
         </li>
@@ -45,7 +46,7 @@ function Tags() {
       <Space/>
       <Space/>
       <Center>
-      <Button>新增标签</Button>
+      <Button onClick={addTag}>新增标签</Button>
       </Center>
     </Layout>
 
